a6b716f6556f728ff0a61c70010a7465eb2791bb
Oyiborhoro Benedicta <ladyfsquare@yahoo.com>
2023-07-25 13:40:38 -0700

    modified

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,22 +1,15 @@
-// All your functions must be exported
 export function taskFirst() {
-  // function taskFirst to instantiate variables using const
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Exporting function getLast
 export function getLast() {
   return ' is okay';
 }
 
-// All your functions must be exported
 export function taskNext() {
-  // function taskNext to instantiate variables using let
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 }
-
-
